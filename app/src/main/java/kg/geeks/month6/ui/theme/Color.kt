ae3f577112a1a05e4d1b64b610f0fc3346141c3a
Kulubekov Alisher <kulubekovalish@icloud.com>
2025-02-04 17:35:08 +0600

    Home Work 1

--- app/src/main/java/kg/geeks/month6/ui/theme/Color.kt
+++ app/src/main/java/kg/geeks/month6/ui/theme/Color.kt
@@ -2,6 +2,18 @@ package kg.geeks.month6.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val BluePrimary = Color(0xFF0000FF) // Основной синий
+val BlueDark = Color(0xFF00008B) // Тёмно-синий
+val BlueLight = Color(0xFF00BFFF) // Голубой
+val BlueSky = Color(0xFF87CEEB) // Небесно-голубой
+val BlueRoyal = Color(0xFF4169E1) // Королевский синий
+val BlueCobalt = Color(0xFF0047AB) // Кобальтовый синий
+val BlueElectric = Color(0xFF7DF9FF) // Электрик синий
+val BlueIndigo = Color(0xFF4B0082) // Индиго
+val BlueTurquoise = Color(0xFF40E0D0) // Бирюзовый
+val BlueAzure = Color(0xFF007FFF) // Лазурный
+val DefaultBlueColor = Color(0xFF337180)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +21,4 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
